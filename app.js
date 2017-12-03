b29487b0f8d2e72615cc3b80b537eaf072d92df2
Akshay Soam <akshaysoam8@gmail.com>
2017-12-03 15:25:16 +0530

    get car and post cars mapping added

--- app.js
+++ app.js
@@ -22,6 +22,39 @@ server.listen(process.env.port || 80, function(err) {
     console.log(err ? err : chalk.green('Server is running', new Date()));
 });
 
+app.post('/car', function(req, res) {
+    var data = req.body.year + ', ' + req.body.model + '\n';
+
+    fs.appendFile('data.csv', data, function(err) {
+        if(err) {
+            console.log(err);
+            res.send(500);
+        }
+
+        else
+            res.send(200);
+    });
+});
+
+app.get('/cars', function (req, res) {
+    var lines = fs.readFileSync('data.csv').toString().split('\n');
+    var result = new Array();
+
+    lines.forEach(function(line) {
+        if(line.length == 0)
+            return;
+
+        var split = line.split(', ');
+
+        result.push({
+            year : split[0],
+            model : split[1]
+        });
+    });
+
+    res.send(result);
+});
+
 app.get('*', function(req, res) {
     res.sendfile('index.html');
 });
